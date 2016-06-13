b8f5c68d57fb64b87709dc88d9fdb8929482b9c8
Sharpmaxim <maximsoolkovskiy@gmail.com>
2016-06-14 01:37:57 +0300

    update

--- StringReverse.py
+++ StringReverse.py
@@ -1,4 +1,27 @@
 def get_string():
     return input("Please enter a string of characters: ")
 
-def
+
+def reverse_string(sentence):
+    chars = [None] * len(sentence)
+    index = len(sentence) - 1
+    for n in range(len(sentence)):
+        chars[index] = sentence[n]
+        index -= 1
+    return char_to_string(chars)
+
+
+def char_to_string(chars):
+    sentence = ""
+    for n in range(len(chars)):
+        sentence += chars[n]
+    return sentence
+
+
+def display_result(reversed_sentence):
+    print("The reversed sentence:\n" + reversed_sentence)
+
+
+sentence = get_string()
+reversed_sentence = reverse_string(sentence)
+display_result(reversed_sentence)
